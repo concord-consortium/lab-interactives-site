3775664567c87a2c4c0766ce9d7b0c38afbc557c
Piotr Janik <janikpiotrek@gmail.com>
2014-03-18 23:12:15 +0100

    Optimize script for creating archived public dir
    [#67398842]

--- script/create-archived-public-dir.sh
+++ script/create-archived-public-dir.sh
@@ -6,7 +6,7 @@
 version=$1
 if [ -z "$2" ]
   then
-    lab_path=`../app/public/lab`
+    lab_path="../app/public/lab"
   else
     lab_path=$2
 fi
@@ -22,11 +22,11 @@ echo "- copy public to version/$version/public/"
 # .git - public folder used to be a git repo (previous version of this script was creating it),
 # version - symbolic links to other versions,
 # jnlp - following convention of the previous script, we also don't want to include that in archive.
-rsync -a --exclude='.git/' --exclude='version/' --exclude='jnlp/' public version/$version/
+rsync -aq --exclude='.git/' --exclude='version/' --exclude='jnlp/' public version/$version/
 
 echo "- copy $lab_path to version/$version/public/"
 
-rsync -a $lab_path version/$version/public/
+rsync -aq $lab_path version/$version/public/
 
 echo "- remove unnecessary HTML pages"
 
@@ -39,24 +39,18 @@ rm version/$version/public/embeddable-staging.html
 rm version/$version/public/embeddable-dev.html
 rm version/$version/public/embeddable-local.html
 
-echo "- prepare temp directory to create $archivename"
+echo "- generate HTML pages with correct Lab root URL and STATIC=true"
 
-rsync -a version/$version/public/ version/$version/$version/
-
-echo "- generate HTML pages with correct Lab root URL"
-
-# Generate pages for versioned directory.
-LAB_ROOT_URL="lab" bin/haml -r ./script/setup.rb src/interactives.html.haml version/$version/public/interactives.html
+LAB_ROOT_URL="lab" LAB_STATIC=true bin/haml -r ./script/setup.rb src/interactives.html.haml version/$version/public/interactives.html
 LAB_ROOT_URL="lab" bin/haml -r ./script/setup.rb src/embeddable.html.haml version/$version/public/embeddable.html
-
-# Generate pages for static, versioned directory.
-LAB_ROOT_URL="lab" LAB_STATIC=true bin/haml -r ./script/setup.rb src/interactives.html.haml version/$version/$version/interactives.html
-LAB_ROOT_URL="lab" bin/haml -r ./script/setup.rb src/embeddable.html.haml version/$version/$version/embeddable.html
 
 echo "- generate $archivename archive"
 
+mv version/$version/public/ version/$version/$version/
 tar -zcf version/$archivename --directory=version/$version/ $version
+mv version/$version/$version/ version/$version/public/
 
-echo "- cleanup"
+echo "- generate HTML pages with correct Lab root URL and STATIC=false"
 
-rm -rf version/$version/$version/
+LAB_ROOT_URL="lab" bin/haml -r ./script/setup.rb src/interactives.html.haml version/$version/public/interactives.html
+LAB_ROOT_URL="lab" bin/haml -r ./script/setup.rb src/embeddable.html.haml version/$version/public/embeddable.html
