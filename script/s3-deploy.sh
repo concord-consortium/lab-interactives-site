fc76b4bb1a7dbbab6243baa13aadf9a9a290c01f
Piotr Janik <janikpiotrek@gmail.com>
2014-04-24 22:10:14 +0200

    Add gzip support to S3 deploy

--- script/s3-deploy.sh
+++ script/s3-deploy.sh
@@ -34,10 +34,37 @@ if [ "$TRAVIS_BRANCH" = "s3cmd-upload" ]; then
 
   echo "- generate archive _site/version/$SITE_VERSION.tar.gz"
   tar -czf _site/version/$SITE_VERSION.tar.gz --directory=_site/version/ $SITE_VERSION
-  s3cmd sync -c config/s3cmd --delete-removed --no-preserve --rexclude '^(version/(?!'"$SITE_VERSION"')|branch/)' _site/ s3://interactives-s3.concord.org/
 else
   echo "deploying $TRAVIS_BRANCH branch: updating branch/$TRAVIS_BRANCH"
   mkdir -p _site/branch
-  cp -R public _site/branch/$TRAVIS_BRANCH
-  s3cmd sync -c config/s3cmd --delete-removed --no-preserve --exclude '*' --rinclude '^branch/'"$TRAVIS_BRANCH"'/' _site/ s3://interactives-s3.concord.org/
+  echo "- move public to _site/branch/$TRAVIS_BRANCH"
+  mv public _site/branch/$TRAVIS_BRANCH
+fi
+
+echo "- gzip HTML, JS, CSS and JSON files"
+find _site/ -type f -iname '*.html' -exec gzip {} \; -exec mv {}.gz {} \;
+find _site/ -type f -iname '*.js' -exec gzip {} \; -exec mv {}.gz {} \;
+find _site/ -type f -iname '*.css' -exec gzip {} \; -exec mv {}.gz {} \;
+find _site/ -type f -iname '*.json' -exec gzip {} \; -exec mv {}.gz {} \;
+
+if [ "$TRAVIS_BRANCH" = "s3cmd-upload" ]; then
+  inc='^(?!(version/(?!'"$SITE_VERSION"')|branch/))'
+  echo "- sync gzipped content"
+  s3cmd sync -c config/s3cmd --delete-removed --no-preserve --add-header 'Content-Encoding:gzip' --exclude '*' \
+    --rinclude "$inc"'.+\.html' --rinclude "$inc"'.+\.js' --rinclude "$inc"'.+\.css' --rinclude "$inc"'.+\.json' \
+    _site/ s3://interactives-s3.concord.org/
+  echo "- sync non-gzipped content"
+  s3cmd sync -c config/s3cmd --delete-removed --no-preserve --rexclude '^(version/(?!'"$SITE_VERSION"')|branch/)' \
+    --exclude '*.html' --exclude '*.js' --exclude '*.css' --exclude '*.json' \
+    _site/ s3://interactives-s3.concord.org/
+else
+  inc='^branch/'"$TRAVIS_BRANCH"'/'
+  echo "- sync gzipped content"
+  s3cmd sync -c config/s3cmd --delete-removed --no-preserve --add-header 'Content-Encoding:gzip' --exclude '*' \
+    --rinclude "$inc"'.+\.html' --rinclude "$inc"'.+\.js' --rinclude "$inc"'.+\.css' --rinclude "$inc"'.+\.json' \
+    _site/ s3://interactives-s3.concord.org/
+  echo "- sync non-gzipped content"
+  s3cmd sync -c config/s3cmd --delete-removed --no-preserve --rexclude '^(?!branch/'"$TRAVIS_BRANCH"'/)' \
+    --exclude '*.html' --exclude '*.js' --exclude '*.css' --exclude '*.json' \
+    _site/ s3://interactives-s3.concord.org/
 fi
