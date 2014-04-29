111c76fb8e2e9ddad5f81c222055cae25d156f4f
Scott Cytacki <scytacki@concord.org>
2014-04-28 20:25:35 -0400

    fix standalone builder
    it was broken after update to s3 deployed lab

--- script/popluate-standalone.sh
+++ script/popluate-standalone.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 #
-# script/populate-standalone.sh [path-to-lab-framework-dist]
+# script/populate-standalone.sh [environment]
 #
 
-if [ -z "$1" ]
-  then
-    lab_path="../app/public/lab"
-  else
-    lab_path=$1
+if [ -z "$1" ]; then
+  lab_env="default"
+else
+  lab_env=$1
 fi
 
-mkdir -p pubic/standalone/lab-interactive
+mkdir -p public/standalone/lab-interactive
 
-echo "- copy $lab_path to public/standalone/lab-interactive"
+lab_url=`script/lab-root-url.rb $lab_env`
+echo "- download the $lab_env archive at $lab_url.tar.gz and uncompress to public/standalone/lab-interactive"
 
-rsync -aq $lab_path public/standalone/lab-interactive
+curl -O $lab_url.tar.gz
+tar xzf lab.tar.gz -C public/standalone/lab-interactive
+rm lab.tar.gz
 
 echo "- copy necessary files from public to public/standalone/"
 
@@ -24,6 +26,7 @@ done
 
 echo "- copy necessary vendor folders from public to public/standalone/lab-interactive"
 
+mkdir -p public/standalone/lab-interactive/vendor
 VENDOR_FOLDERS="modernizr shutterbug"
 for vendor_folder_to_copy in $VENDOR_FOLDERS; do
 	cp -r public/vendor/$vendor_folder_to_copy public/standalone/lab-interactive/vendor/
