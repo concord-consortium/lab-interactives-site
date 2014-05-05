14bb0817c238a8437b587e35495f945a85f9999a
Piotr Janik <janikpiotrek@gmail.com>
2014-05-05 15:56:11 +0200

    Fix branch detection on Travis CI
    [#69707496]

--- script/branch-config-file.rb
+++ script/branch-config-file.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 CONFIG_PATH = File.join(File.expand_path('../..',  __FILE__), 'config')
 
-head = File.open('.git/HEAD') do |f|
-         f.read
-       end
-head.delete!("\n").slice!('ref: refs/heads/')
-branch_config = File.join(CONFIG_PATH, "#{head}.yml")
+branch = ENV['TRAVIS_BRANCH']
+if branch.nil?
+  branch = File.open('.git/HEAD') { |f| f.read }.delete!("\n")
+  branch.slice!('ref: refs/heads/')
+end
+branch_config = File.join(CONFIG_PATH, "#{branch}.yml")
 print File.file?(branch_config) ? branch_config : File.join(CONFIG_PATH, 'master.yml')
