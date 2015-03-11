4321d3df4115982e284b9a9a3dadb1b241e30c56
Scott Cytacki <scytacki@concord.org>
2015-03-11 10:56:45 -0400

    Upgrade production to LabFramework-1.5.1 (Take 2)

--- script/setup.rb
+++ script/setup.rb
@@ -65,8 +65,8 @@ CONFIG = YAML.load_file(branch_config)
 config_lab_root_url = CONFIG[:lab_root_url] || {}
 
 LAB_ROOT_URL = {
-  :default     => config_lab_root_url[:default]     || "//lab-framework.concord.org/version/1.4.0/lab",
-  :production  => config_lab_root_url[:production]  || "//lab-framework.concord.org/version/1.4.0/lab",
+  :default     => config_lab_root_url[:default]     || "//lab-framework.concord.org/version/1.5.1/lab",
+  :production  => config_lab_root_url[:production]  || "//lab-framework.concord.org/version/1.5.1/lab",
   :staging     => config_lab_root_url[:staging]     || "//lab-framework.concord.org/version/1.5.1/lab",
   :development => config_lab_root_url[:development] || "//lab-framework.concord.org/branch/master/lab",
   :local       => config_lab_root_url[:local]       || "//localhost:9191/lab"
