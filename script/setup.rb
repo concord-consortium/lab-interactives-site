f0c54bc6e5b13e1bb0865a355527514960984b50
Piotr Janik <janikpiotrek@gmail.com>
2015-08-14 12:41:09 +0200

    Update Lab to 1.7.0

--- script/setup.rb
+++ script/setup.rb
@@ -65,9 +65,9 @@ CONFIG = YAML.load_file(branch_config)
 config_lab_root_url = CONFIG[:lab_root_url] || {}
 
 LAB_ROOT_URL = {
-  :default     => config_lab_root_url[:default]     || "//lab-framework.concord.org/version/1.6.0/lab",
-  :production  => config_lab_root_url[:production]  || "//lab-framework.concord.org/version/1.6.0/lab",
-  :staging     => config_lab_root_url[:staging]     || "//lab-framework.concord.org/version/1.7.0-pre.2/lab",
+  :default     => config_lab_root_url[:default]     || "//lab-framework.concord.org/version/1.7.0/lab",
+  :production  => config_lab_root_url[:production]  || "//lab-framework.concord.org/version/1.7.0/lab",
+  :staging     => config_lab_root_url[:staging]     || "//lab-framework.concord.org/version/1.7.0/lab",
   :development => config_lab_root_url[:development] || "//lab-framework.concord.org/branch/master/lab",
   :local       => config_lab_root_url[:local]       || "//localhost:9191/lab"
 }
