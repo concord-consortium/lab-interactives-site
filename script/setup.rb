8d13092b208d78fdf6caad353f2a91254bbb9cee
pjanik <janikpiotrek@gmail.com>
2020-04-29 19:14:43 +0200

    Update Lab staging to 1.17.1

--- script/setup.rb
+++ script/setup.rb
@@ -67,7 +67,7 @@ config_lab_root_url = CONFIG[:lab_root_url] || {}
 LAB_ROOT_URL = {
   :default     => config_lab_root_url[:default]     || "//lab-framework.concord.org/version/1.17.0/lab",
   :production  => config_lab_root_url[:production]  || "//lab-framework.concord.org/version/1.17.0/lab",
-  :staging     => config_lab_root_url[:staging]     || "//lab-framework.concord.org/version/1.17.0/lab",
+  :staging     => config_lab_root_url[:staging]     || "//lab-framework.concord.org/version/1.17.1/lab",
   :development => config_lab_root_url[:development] || "//lab-framework.concord.org/branch/master/lab",
   :local       => config_lab_root_url[:local]       || "//localhost:9191/lab"
 }
